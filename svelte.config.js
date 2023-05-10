950e8da68eb9ac956e7b7b77fada1ccd37104097
Rohan Godha <rgodha24@brophybroncos.org>
2023-05-10 09:49:08 -0700

    switch to sst + aws

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
+import adapter from 'svelte-kit-sst';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter()
 	}
